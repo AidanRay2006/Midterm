ff520b7c4db0c2fb64f56259bd78c283eeaa83a0
AidanRay2006 <aray5@ltu.edu>
2025-10-17 13:46:14 -0400

    The bike accelerates and the camera follows it
    The idea to keep the bike grounded was partly provided by ChatGPT.

---
+++ Assets/_Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject follow;
+    public float min = -491;
+    public float max = 491;
+
+    private Vector3 followPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //starts the camera at the current positon of the object its following
+        //but only its x
+        followPos = new Vector3(follow.transform.position.x, transform.position.y, transform.position.z);
+        transform.position = followPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gets the position of the object it's following every frame
+        Vector3 followPos = new Vector3(follow.transform.position.x, transform.position.y, transform.position.z);
+        
+        //the camera only follows the object if it is within certain bounds
+        if (transform.position.x < max)
+        {
+            transform.position = followPos;
+        }
+    }
+}
---
+++ Assets/_Scripts/PlayerBike.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBike : MonoBehaviour
+{
+    //public variables
+    public float speed = 0f;
+    public float maxSpeed = 55f;
+    public float acc = 2f;
+
+    //private variables
+    private Rigidbody rb;
+    private bool onGround;
+    private bool flip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        speed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = rb.velocity.x;
+        onGround = Grounded();
+        flip = Flipped();
+        if (flip)
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+        if(speed < 16.5f)
+        {
+            acc = 35f;
+        }
+        else
+        {
+            acc = 25f;
+        }
+        if (Input.GetKey(KeyCode.Period) && speed < maxSpeed && onGround)
+        {
+            rb.AddForce(Vector3.right * acc, ForceMode.Acceleration);
+        }
+        else if (speed >= maxSpeed)
+        {
+            rb.velocity = new Vector3(maxSpeed, 0);
+        }
+
+    }
+
+    public bool Grounded()
+    {
+        if (Physics.Raycast(transform.position, -transform.up, 0.5f))
+        {
+            return true;
+        }
+        return false;
+    }
+    private bool Flipped()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, 0.5f) && transform.eulerAngles.z != 0)
+        {
+            return true;
+        }
+        return false;
+    }
+}
