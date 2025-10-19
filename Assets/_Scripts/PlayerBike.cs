7283f0a9fbe9047ed586a8884554671cede47bce
AidanRay2006 <aray5@ltu.edu>
2025-10-19 14:21:55 -0400

    Added temp gauge

--- Assets/_Scripts/PlayerBike.cs
+++ Assets/_Scripts/PlayerBike.cs
@@ -1,25 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerBike : MonoBehaviour
 {
     //public variables
     public float speed = 0f;
-    public float maxSpeed = 55f;
-    public float acc = 2f;
-    public float strafeAcc = 2f;
+    public float maxSpeed = 22f;
+    public float acc = 25f;
+    public float strafeAcc = 25f;
+    public float temp;
+    public float maxTempSpeed = 30;
+    public float tempAcc = 35;
+    public float tempIncreaseRate = 20f;
+    public float tempDecreaseRate = 10f;
+    public float maxTemp = 1000f;
 
     //private variables
     private Rigidbody rb;
     private bool onGround;
     private bool flip;
+    private bool recharge;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         speed = 0;
+        recharge = false;
     }
 
     // Update is called once per frame
@@ -49,10 +58,29 @@ public class PlayerBike : MonoBehaviour
         if (Input.GetKey(KeyCode.Period) && speed < maxSpeed && onGround)
         {
             rb.AddForce(Vector3.right * acc, ForceMode.Acceleration);
+            //keep it within the max speed while accelerating
+            if (speed >= maxSpeed)
+            {
+                //lower the max speed when the temp gauge is recharging
+                if (recharge)
+                {
+                    rb.velocity = new Vector3(maxSpeed - 5, rb.velocity.y);
+                }
+                rb.velocity = new Vector3(maxSpeed, rb.velocity.y);
+            }
         }
-        else if (speed >= maxSpeed)
+       
+        //accelerate via temp
+        if (Input.GetKey(KeyCode.Comma) && speed < maxTempSpeed && onGround && !recharge)
         {
-            rb.velocity = new Vector3(maxSpeed, 0);
+            //increase the temp gauge when using the special acceleration
+            temp += tempIncreaseRate;
+            rb.AddForce(Vector3.right * tempAcc, ForceMode.Acceleration);
+            //keep it within the max speed while accelerating
+            if (speed >= maxTempSpeed)
+            {
+                rb.velocity = new Vector3(maxTempSpeed, 0);
+            }
         }
 
         //move up and down
@@ -65,10 +93,44 @@ public class PlayerBike : MonoBehaviour
             rb.AddForce(Vector3.back * strafeAcc, ForceMode.Force);
         }
 
+        //if the player has maxed out the limit, start recharging and penalize them
+        if(temp >= maxTemp)
+        {
+            temp = maxTemp;
+            recharge = true;
+        }
+
+        //decrease the gauge
+        if (!Input.GetKey(KeyCode.Comma))
+        {
+            temp -= tempDecreaseRate;
+        }
+        //keep temp from becoming negative
+        if (temp < 0)
+        {
+            temp = 0;
+        }
+
+        //The temp gauge has reached 0, so it is no longer recharging
+        if (temp == 0)
+        {
+            recharge = false;
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
-        transform.position = new Vector3(-425.2f, transform.position.y, transform.position.z);
+        if (other.CompareTag("Portal"))
+        {
+            transform.position = new Vector3(-425.2f, transform.position.y, transform.position.z);
+        }
+        if (other.CompareTag("Mud"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x - 20, rb.velocity.y, rb.velocity.z);
+        }
+        if (other.CompareTag("Arrow"))
+        {
+            temp = 0;
+        }
     }
 
     public bool Grounded()
